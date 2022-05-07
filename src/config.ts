52d304fa973af35643729eab9ccdc794138b1141
tekuteku11udana <tekuteku11udana@gmail.com>
2022-05-07 22:28:56 +0900

    change env var

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config = {
     wsserver: {
         // url: process.env.WS_SERVER_URI as string || "ws://localhost:3001"
-        url: process.env.NODE_ENV === "production" ? String(process.env.REACT_APP_WS_SERVER_URI) : "ws://localhost:3001",
+        url: process.env.NODE_ENV === "production" ? "https://collab-note-yjs-backend.herokuapp.com" : "ws://localhost:3001",
     },
     SOCKET_RECONNECTION_TIMEOUT: 1000,
 }
