438619f1f069bce1380749f727a6f1ea029f7270
tekuteku11udana <tekuteku11udana@gmail.com>
2022-05-08 12:33:19 +0900

    change server url.

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ const config = {
         // url: process.env.WS_SERVER_URI as string || "ws://localhost:3001"
         // url: "ws://localhost:3001"
         // url: process.env.NODE_ENV === "production" ? "wss://collab-note-yjs-backend.herokuapp.com" : "ws://localhost:3001",
-        url: "ws://collab-note-yjs-backend.herokuapp.com"
-        // url: "wss://collab-note-yjs-backend.herokuapp.com"
+        // url: "ws://collab-note-yjs-backend.herokuapp.com"
+        url: "wss://collab-note-yjs-backend.herokuapp.com"
     },
     SOCKET_RECONNECTION_TIMEOUT: 1000,
 }
