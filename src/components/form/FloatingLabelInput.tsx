32b77b23910c84cdbf4275987dadccbe15a3faa5
tekuteku11udana <tekuteku11udana888@gmail.com>
2022-06-12 11:37:06 +0900

    Fix FloatingLabelInput styling.

--- src/components/form/FloatingLabelInput.tsx
+++ src/components/form/FloatingLabelInput.tsx
@@ -5,15 +5,15 @@ export const FloatingLabelInput = (props: {
   onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
 }) => {
   return (
-    <div className="relative h-14">
+    <div className="relative h-12 w-full">
       <input
-        className="peer relative top-3.5 bg-white/0 border-gray-300 border-b-2 w-full focus:outline-none focus:border-rose-600 placeholder-transparent"
+        className="peer absolute bottom-0 bg-white/0 border-gray-300 border-b-2 w-full h-8 focus:outline-none focus:border-rose-600 placeholder-transparent"
         type={props.type}
         placeholder="invisible but needed"
         value={props.value}
         onChange={props.onChange}
       />
-      <label className="absolute left-0 peer-focus:top-0  transition-all peer-focus:text-gray-600 peer-focus:text-sm peer-placeholder-shown:text-gray-400 peer-placeholder-shown:text-base peer-placeholder-shown:top-3.5 -z-10">
+      <label className="absolute left-0 top-0 text-gray-600 text-sm peer-focus:top-0   peer-focus:text-gray-600 peer-focus:text-sm peer-placeholder-shown:text-gray-400 peer-placeholder-shown:text-base peer-placeholder-shown:top-4 transition-all -z-10">
         {props.label}
       </label>
     </div>
