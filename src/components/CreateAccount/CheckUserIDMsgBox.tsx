a3e39eff28ef215b5f752b05d4cf60431ba7c422
tekuteku11udana <tekuteku11udana888@gmail.com>
2022-06-17 20:44:32 +0900

    Add simple error handling in CheckUserIDMsgBox.

--- src/components/CreateAccount/CheckUserIDMsgBox.tsx
+++ src/components/CreateAccount/CheckUserIDMsgBox.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { isThrownErr } from "../../api/base";
 import { useCheckUserID } from "../../api/hooks";
 import { Validate } from "../../utils/validation";
 import { CheckSvg } from "./CheckSvg";
@@ -27,7 +28,7 @@ export const CheckUserIDMsgBox = (props: { userID: string }) => {
 
   return (
     <div
-      className="w-full h-6 flex flex-row gap-3 border-b-2"
+      className="w-full flex flex-col border-b-2"
       style={{
         backgroundColor: boxStatus === "NG" ? "red" : "white",
         color:
@@ -35,24 +36,38 @@ export const CheckUserIDMsgBox = (props: { userID: string }) => {
         fontWeight: boxStatus === "NG" ? "bold" : "normal",
       }}
     >
-      <div className="flex-none w-6">
-        <CheckSvg status={boxStatus === "loading" ? "disabled" : boxStatus} />
-      </div>
+      <div className="w-full h-6 flex flex-row gap-3">
+        <div className="flex-none w-6">
+          <CheckSvg status={boxStatus === "loading" ? "disabled" : boxStatus} />
+        </div>
 
-      <div className="flex-grow">unique ID? </div>
+        <div className="flex-grow">unique ID? </div>
 
-      {/* {!props.errMsg ? null : typeof props.errMsg !== "string" ? (
+        {/* {!props.errMsg ? null : typeof props.errMsg !== "string" ? (
             props.errMsg
           ) : (
             <div className="border-2">{props.errMsg}</div>
           )} */}
 
-      <div className="mx-3">
-        {boxStatus === "loading" ? (
-          "Loading..."
-        ) : boxStatus === "NG" ? (
-          <ExclamationSvg />
-        ) : null}
+        <div className="mx-3">
+          {boxStatus === "loading" ? (
+            "Loading..."
+          ) : boxStatus === "NG" ? (
+            <ExclamationSvg />
+          ) : null}
+        </div>
+      </div>
+
+      <div className="pl-9">
+        {boxStatus !== "NG" || status === 200
+          ? null
+          : status === 403
+          ? "already used"
+          : isThrownErr(status)
+          ? status
+          : status === 500
+          ? "500 Internal Server Error"
+          : "400 Bad Request: If you see this message, please tell us!"}
       </div>
     </div>
   );
