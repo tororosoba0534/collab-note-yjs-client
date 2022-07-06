71efc21ed649971b5aa54560014a6565987e765a
tekuteku11udana <tekuteku11udana888@gmail.com>
2022-07-06 17:57:12 +0900

    Create simple LP.

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,25 @@
+import { useNavigate } from "react-router-dom";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="flex flex-col justify-center gap-10 text-center">
+      <h1>Collab Note YJS</h1>
+      <p>enables realtime collaborative notetaking with YJS</p>
+      <button
+        className="px-4 py-2 rounded bg-rose-500 hover:bg-rose-400 text-white font-semibold text-center block w-full focus:outline-none focus:ring focus:ring-offset-2 focus:ring-rose-500 focus:ring-opacity-80 cursor-pointer"
+        onClick={() => navigate("/login")}
+      >
+        Login
+      </button>
+      <button
+        className="px-4 py-2 rounded bg-rose-500 hover:bg-rose-400 text-white font-semibold text-center block w-full focus:outline-none focus:ring focus:ring-offset-2 focus:ring-rose-500 focus:ring-opacity-80 cursor-pointer"
+        onClick={() => navigate("/create-account")}
+      >
+        Create new account
+      </button>
+    </div>
+  );
 };
 
 export default HomePage;
