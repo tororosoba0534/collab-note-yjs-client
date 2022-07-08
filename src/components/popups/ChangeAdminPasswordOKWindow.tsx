7acc57f4bc998327387257b4bb84ec5d67711ed4
tekuteku11udana <tekuteku11udana888@gmail.com>
2022-07-08 23:22:25 +0900

    Stop reloading when changeAdminPassword and changePassword. Button in ConnStatusBox enables to reload if connection failed.

--- src/components/popups/ChangeAdminPasswordOKWindow.tsx
+++ src/components/popups/ChangeAdminPasswordOKWindow.tsx
@@ -1,13 +1,16 @@
+import { useContext } from "react";
+import { PersonalContext } from "../personal/PersonalContext";
 import { PopupTemplate } from "./PopupTemplate";
 
 export const ChangeAdminPasswordOKWindow = () => {
+  const { setPopupStatus } = useContext(PersonalContext);
   return (
     <PopupTemplate handleClose={null}>
       <div>Admin password changed.</div>
 
       <button
         className="border-2 border-gray-400 rounded-md px-2 mx-4 hover:bg-rose-200"
-        onClick={() => window.location.reload()}
+        onClick={() => setPopupStatus(null)}
       >
         OK
       </button>
--- src/components/popups/ChangePasswordOKWindow.tsx
+++ src/components/popups/ChangePasswordOKWindow.tsx
@@ -1,13 +1,16 @@
+import { useContext } from "react";
+import { PersonalContext } from "../personal/PersonalContext";
 import { PopupTemplate } from "./PopupTemplate";
 
 export const ChangePasswordOKWindow = () => {
+  const { setPopupStatus } = useContext(PersonalContext);
   return (
     <PopupTemplate handleClose={null}>
       <div>Password changed.</div>
 
       <button
         className="border-2 border-gray-400 rounded-md px-2 mx-4 hover:bg-rose-200"
-        onClick={() => window.location.reload()}
+        onClick={() => setPopupStatus(null)}
       >
         OK
       </button>
