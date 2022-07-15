e01fc0051ad6d3ffe87df17b7df4d3ccf01a4aea
tekuteku11udana <tekuteku11udana888@gmail.com>
2022-07-15 19:20:34 +0900

    Succeed in applying overflow scroll CSS to PopupTemplate.

--- src/components/popups/PopupTemplate.tsx
+++ src/components/popups/PopupTemplate.tsx
@@ -11,17 +11,19 @@ export const PopupTemplate = (props: {
         e.stopPropagation();
       }}
     >
-      <div className="bg-gray-100 rounded-lg relative flex flex-col justify-center items-center w-72">
+      <div className="bg-gray-100 rounded-lg  w-72 h-[80%]">
         {props.handleClose ? (
-          <div
-            className="absolute top-0 right-0 w-5 h-5 hover:font-bold cursor-pointer"
-            onClick={props.handleClose}
-          >
-            X
+          <div className="relative w-full h-5">
+            <div
+              className="absolute top-0 right-0 w-5 h-5 hover:font-bold cursor-pointer"
+              onClick={props.handleClose}
+            >
+              X
+            </div>
           </div>
         ) : null}
-        <div className="p-5 pt-10 flex flex-col justify-center items-center">
-          {props.children}
+        <div className="p-5 py-10 w-full h-full flex flex-col ">
+          <div className="flex-1 overflow-auto">{props.children}</div>
         </div>
       </div>
     </div>
